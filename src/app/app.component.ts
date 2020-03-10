924e8505cfe3592f062922cb6c4d9630f3ec5c13
MiguDeveloper <miguel.peru.seo@gmail.com>
2020-03-10 00:23:11 -0500

    sweetalert completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-angular9';
+
+  verSweetalert(){
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'success',
+      title: 'Configuracion correcta',
+      text: 'Excelente procediste con todo bien configurado',
+    })
+  }
 }
+
+
